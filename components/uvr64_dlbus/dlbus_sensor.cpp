71616d4962b8287ee06685a3142770665cafdd3e
DerTolleMaz <123926194+DerTolleMaz@users.noreply.github.com>
2025-06-19 09:58:53 +0200

    Update dlbus_sensor.cpp

--- components/uvr64_dlbus/dlbus_sensor.cpp
+++ components/uvr64_dlbus/dlbus_sensor.cpp
@@ -25,16 +25,16 @@ void DLBusSensor::update() {
   }
 }
 
-void DLBusSensor::loop() {
-  if (frame_ready_) {
-    ESP_LOGD("uvr64_dlbus", "DLBus frame received, decoding...");
-    parse_frame_();
-    bit_index_ = 0;
-    last_edge_ = micros();
-    attachInterruptArg(digitalPinToInterrupt(pin_), isr, this, CHANGE);
-    frame_ready_ = false;
-  }
-}
+//void DLBusSensor::loop() {
+//  if (frame_ready_) {
+//    ESP_LOGD("uvr64_dlbus", "DLBus frame received, decoding...");
+//    parse_frame_();
+//    bit_index_ = 0;
+//    last_edge_ = micros();
+//    attachInterruptArg(digitalPinToInterrupt(pin_), isr, this, CHANGE);
+//    frame_ready_ = false;
+//  }
+//}
 
 void DLBusSensor::set_temp_sensor(int index, sensor::Sensor *sensor) {
   if (index >= 0 && index < 6)
