0e547f1372804bafb36209affaae41cba75a9cd9
DerTolleMaz <123926194+DerTolleMaz@users.noreply.github.com>
2025-06-19 08:03:49 +0200

    Update dlbus_sensor.h

--- components/uvr64_dlbus/dlbus_sensor.h
+++ components/uvr64_dlbus/dlbus_sensor.h
@@ -9,32 +9,28 @@ namespace uvr64_dlbus {
 
 class DLBusSensor : public PollingComponent {
  public:
-  explicit DLBusSensor(uint32_t update_interval = 10000)
-      : PollingComponent(update_interval) {}
+  DLBusSensor() : PollingComponent(10000) {}  // alle 10 Sekunden
 
   void setup() override;
-  void loop() override;
   void update() override;
+  void loop() override;
 
-void set_temp_sensor(int index, sensor::Sensor *sensor);
-void set_relay_sensor(int index, binary_sensor::BinarySensor *sensor);
+  void set_temp_sensor(int index, sensor::Sensor *sensor);
+  void set_relay_sensor(int index, binary_sensor::BinarySensor *sensor);
 
-  void set_timings(const std::vector<uint32_t> &timings) {
-    for (size_t i = 0; i < timings.size() && i < 128; i++) {
-      timings_[i] = timings[i];
-    }
-    frame_ready_ = true;
+  void set_timings(const std::array<uint32_t, 128> &timings) {
+    this->timings_ = timings;
+    this->frame_ready_ = true;
   }
 
  protected:
   void parse_frame_();
 
+  std::array<uint32_t, 128> timings_;
+  std::array<bool, 256> bits_;
   sensor::Sensor *temp_sensors_[6]{};
   binary_sensor::BinarySensor *relay_sensors_[4]{};
-
-  bool frame_ready_ = false;
-  uint32_t timings_[128]{};
-  bool bits_[256]{};
+  bool frame_ready_{false};
 };
 
 }  // namespace uvr64_dlbus
