264f720a61f5e80405b6ee9c305228dc18d9efb4
DerTolleMaz <123926194+DerTolleMaz@users.noreply.github.com>
2025-06-23 05:53:09 +0200

    Update dlbus_sensor.h

--- components/uvr64_dlbus/dlbus_sensor.h
+++ components/uvr64_dlbus/dlbus_sensor.h
@@ -10,24 +10,26 @@ namespace uvr64_dlbus {
 class DLBusSensor : public Component {
  public:
   explicit DLBusSensor(uint8_t pin);
-
   void setup() override;
   void loop() override;
 
-  void set_temp_sensor(uint8_t index, sensor::Sensor *sensor);
-  void set_relay_sensor(uint8_t index, binary_sensor::BinarySensor *sensor);
+  void set_temp_sensor(uint8_t index, sensor::Sensor *sensor) {
+    if (index < 6) this->temp_sensors_[index] = sensor;
+  }
 
-  static void IRAM_ATTR isr(void *arg);
+  void set_relay_sensor(uint8_t index, binary_sensor::BinarySensor *sensor) {
+    if (index < 4) this->relay_sensors_[index] = sensor;
+  }
 
  protected:
+  static void IRAM_ATTR isr(void *arg);
   void parse_frame_();
   void compute_timing_stats_();
 
   uint8_t pin_;
   sensor::Sensor *temp_sensors_[6]{};
   binary_sensor::BinarySensor *relay_sensors_[4]{};
-
-  volatile bool frame_buffer_ready_{false};
+  volatile bool frame_buffer_ready_ = false;
   uint16_t timing_histogram_[64]{};
 };
 
