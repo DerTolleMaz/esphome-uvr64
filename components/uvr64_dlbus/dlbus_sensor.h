350a7d9242eaff3a9af1afc43c982ecb31da943d
DerTolleMaz <123926194+DerTolleMaz@users.noreply.github.com>
2025-06-23 05:47:16 +0200

    Update dlbus_sensor.h

--- components/uvr64_dlbus/dlbus_sensor.h
+++ components/uvr64_dlbus/dlbus_sensor.h
@@ -1,3 +1,11 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
+
+namespace esphome {
+namespace uvr64_dlbus {
 
 class DLBusSensor : public Component {
  public:
@@ -6,26 +14,21 @@ class DLBusSensor : public Component {
   void setup() override;
   void loop() override;
 
-  void set_temp_sensor(uint8_t index, sensor::Sensor *sensor) {
-    if (index < 6) this->temp_sensors_[index] = sensor;
-  }
+  void set_temp_sensor(uint8_t index, sensor::Sensor *sensor);
+  void set_relay_sensor(uint8_t index, binary_sensor::BinarySensor *sensor);
 
-  void set_relay_sensor(uint8_t index, binary_sensor::BinarySensor *sensor) {
-    if (index < 4) this->relay_sensors_[index] = sensor;
-  }
-
- protected:
   static void IRAM_ATTR isr(void *arg);
 
+ protected:
   void parse_frame_();
   void compute_timing_stats_();
 
   uint8_t pin_;
-
   sensor::Sensor *temp_sensors_[6]{};
   binary_sensor::BinarySensor *relay_sensors_[4]{};
 
-  // Hier kannst du weitere interne Variablen einbauen (Buffer etc.)
+  volatile bool frame_buffer_ready_{false};
+  uint16_t timing_histogram_[64]{};
 };
 
 }  // namespace uvr64_dlbus
