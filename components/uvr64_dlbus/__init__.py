78bd0dee02cbd471b0e341d446cc846509fe5e1a
DerTolleMaz <123926194+DerTolleMaz@users.noreply.github.com>
2025-06-23 05:58:16 +0200

    Update __init__.py

--- components/uvr64_dlbus/__init__.py
+++ components/uvr64_dlbus/__init__.py
@@ -1,26 +1,32 @@
-from esphome.codegen import register_component
+import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome import automation
+from esphome.components import sensor, binary_sensor
+from esphome.const import CONF_ID, CONF_PIN
 
 uvr64_dlbus_ns = cg.esphome_ns.namespace("uvr64_dlbus")
 DLBusSensor = uvr64_dlbus_ns.class_("DLBusSensor", cg.Component)
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(DLBusSensor),
-        cv.Required("pin"): cv.pin,
-        cv.Optional("temp_sensors"): cv.ensure_list(cv.use_id(sensor.Sensor)),
-        cv.Optional("relay_sensors"): cv.ensure_list(cv.use_id(binary_sensor.BinarySensor)),
-    }
-)
+CONF_TEMP_SENSORS = "temp_sensors"
+CONF_RELAY_SENSORS = "relay_sensors"
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(DLBusSensor),
+    cv.Required(CONF_PIN): cv.gpio_input_pin,
+    cv.Required(CONF_TEMP_SENSORS): cv.ensure_list(cv.use_id(sensor.Sensor)),
+    cv.Required(CONF_RELAY_SENSORS): cv.ensure_list(cv.use_id(binary_sensor.BinarySensor)),
+})
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID], config["pin"])
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    pin = await cg.gpio_pin_expression(config[CONF_PIN])
+    cg.add(var.set_pin(pin))
 
-    for i, sensor_id in enumerate(config.get("temp_sensors", [])):
-        sens = await cg.get_variable(sensor_id)
-        cg.add(var.set_temp_sensor(i, sens))
+    for i, sens in enumerate(config[CONF_TEMP_SENSORS]):
+        sens_var = await cg.get_variable(sens)
+        cg.add(var.set_temp_sensor(i, sens_var))
 
-    for i, relay_id in enumerate(config.get("relay_sensors", [])):
-        rel = await cg.get_variable(relay_id)
-        cg.add(var.set_relay_sensor(i, rel))
+    for i, sens in enumerate(config[CONF_RELAY_SENSORS]):
+        sens_var = await cg.get_variable(sens)
+        cg.add(var.set_relay_sensor(i, sens_var))
