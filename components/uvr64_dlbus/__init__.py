b922797cb5cc69ae7d69d995d8f98e315fd6efec
DerTolleMaz <123926194+DerTolleMaz@users.noreply.github.com>
2025-06-23 05:45:32 +0200

    Update __init__.py

--- components/uvr64_dlbus/__init__.py
+++ components/uvr64_dlbus/__init__.py
@@ -1,1 +1,14 @@
-from . import dlbus_sensor
+import esphome.config_validation as cv
+import esphome.codegen as cg
+from esphome import pins
+from esphome.const import CONF_ID
+
+uvr64_dlbus_ns = cg.esphome_ns.namespace('uvr64_dlbus')
+DLBusSensor = uvr64_dlbus_ns.class_('DLBusSensor', cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(DLBusSensor),
+    cv.Required("pin"): pins.internal_gpio_input_pin_schema,
+    cv.Required("temp_sensors"): cv.ensure_list(cv.use_id(cg.Sensor)),
+    cv.Required("relay_sensors"): cv.ensure_list(cv.use_id(cg.BinarySensor)),
+})
