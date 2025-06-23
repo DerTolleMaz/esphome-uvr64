3a42050b6e157e5b9af5e0e30e58decf48e624e4
DerTolleMaz <123926194+DerTolleMaz@users.noreply.github.com>
2025-06-23 05:36:54 +0200

    Update dlbus_sensor.h

--- components/uvr64_dlbus/dlbus_sensor.h
+++ components/uvr64_dlbus/dlbus_sensor.h
@@ -1,34 +1,34 @@
 #pragma once
 
-#include "esphome.h"
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
-#include "esphome/core/gpio.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
 
 namespace esphome {
 namespace uvr64_dlbus {
 
 class DLBusSensor : public Component {
  public:
-  DLBusSensor();
-
-  void set_pin(uint8_t pin);
-  void set_temp_sensor(uint8_t index, sensor::Sensor *sensor);
-  void set_relay_sensor(uint8_t index, binary_sensor::BinarySensor *sensor);
+  explicit DLBusSensor(uint8_t pin);
 
   void setup() override;
   void loop() override;
 
+  void set_temp_sensor(uint8_t index, sensor::Sensor *sensor);
+  void set_relay_sensor(uint8_t index, binary_sensor::BinarySensor *sensor);
+
  protected:
-  static void IRAM_ATTR isr(void *arg);
-  void compute_timing_stats_();
   void parse_frame_();
+  void compute_timing_stats_();
+
+  static void IRAM_ATTR isr(void *arg);
 
   uint8_t pin_;
-  volatile uint32_t last_change_us_{0};
+  volatile uint32_t last_interrupt_time_{0};
   std::vector<uint32_t> bit_durations_;
-  sensor::Sensor *temp_sensors_[6] = {nullptr};
-  binary_sensor::BinarySensor *relay_sensors_[4] = {nullptr};
+  std::vector<uint8_t> frame_buffer_;
+  sensor::Sensor *temp_sensors_[6]{};
+  binary_sensor::BinarySensor *relay_sensors_[4]{};
 };
 
 }  // namespace uvr64_dlbus
