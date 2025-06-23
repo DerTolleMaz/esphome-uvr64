9929d9f758330aa04a50a37044108e8aee765053
DerTolleMaz <123926194+DerTolleMaz@users.noreply.github.com>
2025-06-23 05:53:20 +0200

    Update dlbus_sensor.cpp

--- components/uvr64_dlbus/dlbus_sensor.cpp
+++ components/uvr64_dlbus/dlbus_sensor.cpp
@@ -16,29 +16,29 @@ void DLBusSensor::setup() {
 
 void DLBusSensor::loop() {
   if (frame_buffer_ready_) {
-    detachInterrupt(digitalPinToInterrupt(pin_));  // Temporär deaktivieren
+    detachInterrupt(digitalPinToInterrupt(pin_));
     parse_frame_();
     compute_timing_stats_();
-    frame_buffer_ready_ = false;
     attachInterruptArg(digitalPinToInterrupt(pin_), &DLBusSensor::isr, this, CHANGE);
+    frame_buffer_ready_ = false;
   }
 }
 
 void IRAM_ATTR DLBusSensor::isr(void *arg) {
   auto *self = static_cast<DLBusSensor *>(arg);
-  // Hier DL-Bus-Rohdaten erfassen. Derzeit als Dummy.
+  // TODO: Echtzeit-DL-Bus-Decoder hier implementieren.
   self->frame_buffer_ready_ = true;
 }
 
 void DLBusSensor::parse_frame_() {
-  for (int i = 0; i < 6; ++i) {
-    if (temp_sensors_[i]) {
-      temp_sensors_[i]->publish_state(20.0 + i);  // Dummywerte
+  for (int i = 0; i < 6; i++) {
+    if (this->temp_sensors_[i]) {
+      this->temp_sensors_[i]->publish_state(20.0f + i);
     }
   }
-  for (int i = 0; i < 4; ++i) {
-    if (relay_sensors_[i]) {
-      relay_sensors_[i]->publish_state(i % 2 == 0);  // Dummywerte
+  for (int i = 0; i < 4; i++) {
+    if (this->relay_sensors_[i]) {
+      this->relay_sensors_[i]->publish_state(i % 2 == 0);
     }
   }
   ESP_LOGD(TAG, "Parsed DLBus frame (dummy values).");
@@ -46,19 +46,11 @@ void DLBusSensor::parse_frame_() {
 
 void DLBusSensor::compute_timing_stats_() {
   ESP_LOGI(TAG, "Bit length histogram:");
-  for (int i = 0; i < 64; ++i) {
+  for (int i = 0; i < 64; i++) {
     if (timing_histogram_[i] > 0) {
-      ESP_LOGI(TAG, "Length %d us: %d", i, timing_histogram_[i]);
+      ESP_LOGI(TAG, "  Length %d: %d samples", i, timing_histogram_[i]);
     }
   }
-}
-
-void DLBusSensor::set_temp_sensor(uint8_t index, sensor::Sensor *sensor) {
-  if (index < 6) this->temp_sensors_[index] = sensor;
-}
-
-void DLBusSensor::set_relay_sensor(uint8_t index, binary_sensor::BinarySensor *sensor) {
-  if (index < 4) this->relay_sensors_[index] = sensor;
 }
 
 }  // namespace uvr64_dlbus
