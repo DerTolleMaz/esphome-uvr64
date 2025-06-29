46f05acec6b51cbd5ad1f28887e8c05190a2a382
DerTolleMaz <123926194+DerTolleMaz@users.noreply.github.com>
2025-06-29 15:30:38 +0100

    Update dlbus_sensor.h

--- components/uvr64_dlbus/dlbus_sensor.h
+++ components/uvr64_dlbus/dlbus_sensor.h
@@ -1,53 +1,54 @@
 #pragma once
 
 #include "esphome/core/component.h"
+#include "esphome/core/gpio.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
-#include "esphome/core/gpio.h"
 #include <array>
-#include <cstddef>
 
 namespace esphome {
 namespace uvr64_dlbus {
 
+static const uint32_t FRAME_TIMEOUT_US = 5000;
+static const size_t MAX_BITS = 512;
+
 class DLBusSensor : public Component {
  public:
   DLBusSensor();
-  explicit DLBusSensor(uint8_t pin);
-  explicit DLBusSensor(InternalGPIOPin *pin);
-  void set_pin(uint8_t pin) { this->pin_num_ = pin; }
-  void set_pin(InternalGPIOPin *pin) { this->pin_ = pin; }
-  uint8_t get_pin() const;
+  DLBusSensor(uint8_t pin);
+  DLBusSensor(InternalGPIOPin *pin);
+
   void setup() override;
   void loop() override;
+  uint8_t get_pin() const;
 
   void set_temp_sensor(uint8_t index, sensor::Sensor *sensor) {
-    if (index < 6) this->temp_sensors_[index] = sensor;
+    if (index < temp_sensors_.size())
+      temp_sensors_[index] = sensor;
   }
 
   void set_relay_sensor(uint8_t index, binary_sensor::BinarySensor *sensor) {
-    if (index < 4) this->relay_sensors_[index] = sensor;
+    if (index < relay_sensors_.size())
+      relay_sensors_[index] = sensor;
   }
 
  protected:
   static void IRAM_ATTR isr(DLBusSensor *arg);
-  void parse_frame_();
-  void compute_timing_stats_();
 
-  uint8_t pin_num_{0};
-  InternalGPIOPin *pin_{nullptr};
-  ISRInternalGPIOPin pin_isr_;
-  static constexpr uint32_t FRAME_TIMEOUT_US = 5000;
-  static constexpr size_t MAX_BITS = 256;
-  std::array<uint8_t, MAX_BITS> timings_{};
-  std::array<uint8_t, MAX_BITS> levels_{};
-  size_t bit_index_ = 0;
-  sensor::Sensor *temp_sensors_[6]{};
-  binary_sensor::BinarySensor *relay_sensors_[4]{};
-  volatile bool frame_buffer_ready_ = false;
+  void parse_frame_();
+
+  std::array<uint8_t, MAX_BITS> timings_;
+  std::array<uint8_t, MAX_BITS> levels_;
+  volatile size_t bit_index_{0};
+  volatile bool frame_buffer_ready_{false};
   volatile uint32_t last_change_{0};
-  uint16_t timing_histogram_[64]{};
-  void dump_signal_();
+
+  InternalGPIOPin *pin_{nullptr};
+  uint8_t pin_num_{255};
+  ISRInternalGPIOPin pin_isr_;
+
+  std::array<sensor::Sensor *, 6> temp_sensors_{};
+  std::array<binary_sensor::BinarySensor *, 4> relay_sensors_{};
 };
 
 }  // namespace uvr64_dlbus
