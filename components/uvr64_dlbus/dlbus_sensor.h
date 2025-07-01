2546633e7de89834450990c88b153c022bff7fb0
DerTolleMaz <123926194+DerTolleMaz@users.noreply.github.com>
2025-07-01 09:14:21 +0200

    Update dlbus_sensor.h

--- components/uvr64_dlbus/dlbus_sensor.h
+++ components/uvr64_dlbus/dlbus_sensor.h
@@ -3,22 +3,16 @@
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
-#include <array>
-#include <vector>
 
 namespace esphome {
 namespace uvr64_dlbus {
 
-static const uint32_t FRAME_TIMEOUT_US = 100000;
-static const size_t MAX_BITS = 1024;
-
 class DLBusSensor : public Component {
  public:
-  DLBusSensor() = default;
-  explicit DLBusSensor(uint8_t pin) : pin_num_(pin) {}
+  DLBusSensor(uint8_t pin) : pin_num_(pin) {}
 
   void set_pin(uint8_t pin) { this->pin_num_ = pin; }
-  uint8_t get_pin() const { return this->pin_num_; }
+  uint8_t get_pin() const { return pin_num_; }
 
   void set_temp_sensor(uint8_t index, sensor::Sensor *sensor) { temp_sensors_[index] = sensor; }
   void set_relay_sensor(uint8_t index, binary_sensor::BinarySensor *sensor) { relay_sensors_[index] = sensor; }
@@ -27,22 +21,22 @@ class DLBusSensor : public Component {
   void loop() override;
 
  protected:
-  static void IRAM_ATTR gpio_isr_(DLBusSensor *sensor);
-
   void process_frame_();
-  bool decode_manchester_(std::vector<uint8_t> &result);
   void parse_frame_(const std::vector<uint8_t> &frame);
-
-  void log_bits_();
   void log_frame_(const std::vector<uint8_t> &frame);
+  bool decode_manchester_(std::vector<uint8_t> &result);
+  void log_bits_();
+
+  static void IRAM_ATTR gpio_isr_(DLBusSensor *arg);
 
   uint8_t pin_num_;
-  volatile uint32_t last_change_{0};
-  volatile size_t bit_index_{0};
-  volatile bool frame_buffer_ready_{false};
+  uint32_t last_change_{0};
+  bool frame_buffer_ready_{false};
 
-  std::array<uint8_t, MAX_BITS> timings_{};
-  std::array<uint8_t, MAX_BITS> levels_{};
+  static const int MAX_BITS = 1024;
+  uint8_t levels_[MAX_BITS];
+  uint8_t timings_[MAX_BITS];
+  uint16_t bit_index_{0};
 
   sensor::Sensor *temp_sensors_[6]{};
   binary_sensor::BinarySensor *relay_sensors_[4]{};
