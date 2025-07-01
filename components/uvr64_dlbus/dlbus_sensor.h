89dd9318b75a65170da922b02f6d81bb9657dfc6
DerTolleMaz <123926194+DerTolleMaz@users.noreply.github.com>
2025-07-01 09:06:13 +0200

    Update dlbus_sensor.h

--- components/uvr64_dlbus/dlbus_sensor.h
+++ components/uvr64_dlbus/dlbus_sensor.h
@@ -1,57 +1,50 @@
 #pragma once
+
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
-#include "esphome/core/gpio.h"
+#include "esphome/core/log.h"
+
+#include <vector>
 
 namespace esphome {
 namespace uvr64_dlbus {
 
+static const uint32_t FRAME_TIMEOUT_US = 100000;  // 100ms Frame Timeout
+static const size_t MAX_BITS = 1024;
+
 class DLBusSensor : public Component {
  public:
-  // Konstruktoren
   DLBusSensor() = default;
-  DLBusSensor(uint8_t pin) : pin_num_(pin) {}
-  DLBusSensor(esphome::gpio::InternalGPIOPin *pin) : pin_(pin) {}
+  explicit DLBusSensor(uint8_t pin) : pin_num_(pin) {}
 
-  // Setup & Loop
+  void set_pin(uint8_t pin) { this->pin_num_ = pin; }
+  uint8_t get_pin() const { return this->pin_num_; }
+
+  void set_temp_sensor(uint8_t index, sensor::Sensor *sensor) { temp_sensors_[index] = sensor; }
+  void set_relay_sensor(uint8_t index, binary_sensor::BinarySensor *sensor) { relay_sensors_[index] = sensor; }
+
   void setup() override;
   void loop() override;
 
-  // Pin-Getter/Setter
-  uint8_t get_pin() const {
-    if (this->pin_ != nullptr)
-      return this->pin_->get_pin();
-    return this->pin_num_;
-  }
-
-  void set_pin(uint8_t pin) { this->pin_num_ = pin; }
-  void set_pin(esphome::gpio::InternalGPIOPin *pin) { this->pin_ = pin; }
-
-  // Sensor-Zuweisung
-  void set_temp_sensor(int idx, sensor::Sensor *s) { temp_sensors_[idx] = s; }
-  void set_relay_sensor(int idx, binary_sensor::BinarySensor *s) { relay_sensors_[idx] = s; }
-
  protected:
-  // Interrupt Handler
   static void IRAM_ATTR gpio_isr_(DLBusSensor *sensor);
 
-  // Verarbeitungsroutinen
   void process_frame_();
+  bool decode_manchester_(std::vector<uint8_t> &result);
+  void parse_frame_(const std::vector<uint8_t> &frame);
+
+  void log_bits_();
   void log_frame_(const std::vector<uint8_t> &frame);
 
-  // Pin
-  uint8_t pin_num_{0};
-  esphome::gpio::InternalGPIOPin *pin_{nullptr};
-  esphome::gpio::ISRInternalGPIOPin pin_isr_;
+  uint8_t pin_num_;
+  volatile uint32_t last_change_{0};
+  volatile size_t bit_index_{0};
+  volatile bool frame_buffer_ready_{false};
 
-  // Datenpuffer
-  uint32_t last_change_{0};
-  size_t bit_index_{0};
-  uint8_t levels_[1024]{};
-  uint16_t timings_[1024]{};
+  std::array<uint8_t, MAX_BITS> timings_{};
+  std::array<uint8_t, MAX_BITS> levels_{};
 
-  // Sensoren
   sensor::Sensor *temp_sensors_[6]{};
   binary_sensor::BinarySensor *relay_sensors_[4]{};
 };
